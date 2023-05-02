0efef299c228fe20359c5cc9b57e6836b5016968
Tharindu Yalegama <sd1@crysbro.net>
2023-05-02 16:58:47 +0530

    Add project files.

---
+++ SMSTool/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SMSTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(71, 78);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(268, 20);
+            this.txtPhoneNumber.TabIndex = 0;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(403, 78);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(287, 20);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(97, 138);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtMessage.Size = new System.Drawing.Size(576, 230);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(74, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "To Phone Number :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(93, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Text Message :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(399, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "User Name :";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(348, 391);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "SEND";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ SMSTool/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+using System.Net;
+
+namespace SMSTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            String result;
+            string username = "farmsprideuser@cbs";
+            string digest = "D!2L0g$mScry";
+            string q = "14355541128558";
+            string destination = txtPhoneNumber.Text;
+            string message = txtMessage.Text;
+
+            string url = "https://richcommunication.dialog.lk/api/sms/inline/send?username=" + username + "&digest=" + digest + "&q=" + q + "&destination=" + destination + "&message=" + message;
+
+            StreamWriter smsWriter = null;
+            HttpWebRequest objRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            objRequest.Method = "POST";
+            objRequest.ContentLength = Encoding.UTF8.GetByteCount(url);
+            objRequest.ContentType = "application/x-www-form-urlencoded";
+
+            try
+            {
+                smsWriter = new StreamWriter(objRequest.GetRequestStream());
+                smsWriter.Write(url);
+
+            }
+            catch (Exception ex)
+            {
+               // MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Console.WriteLine(ex.Message);
+
+            }
+            finally
+            {
+                smsWriter.Close();
+
+            }
+
+            //try
+            //{
+
+
+
+
+            //    ////This is a User Name Code.....
+            //    //SerialPort sp = new SerialPort();
+            //    //sp.PortName = txtUserName.Text;
+            //    //sp.Open();
+            //    //sp.WriteLine("AI" + Environment.NewLine);
+            //    //Thread.Sleep(100);
+
+            //    //sp.WriteLine("AT+CMGF=1" + Environment.NewLine);
+            //    //Thread.Sleep(100);
+
+            //    //sp.WriteLine("AT+CMGS=\"" + txtPhoneNumber.Text + "\"" + Environment.NewLine);
+            //    //Thread.Sleep(100);
+
+            //    //sp.WriteLine(txtMessage.Text + Environment.NewLine);
+            //    //Thread.Sleep(100);
+
+
+            //    //sp.Write(new byte[] { 26 }, 0, 1);
+            //    //Thread.Sleep(100);
+
+            //    //var response = sp.ReadExisting();
+            //    //if (response.Contains("ERROR"))
+            //    //    MessageBox.Show("Send failed", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //    //else
+            //    //MessageBox.Show("Message has been sent", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //    //    sp.Close();
+
+            //}
+            //catch (Exception ex){
+            //    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //}
+
+        }
+    }
+}
